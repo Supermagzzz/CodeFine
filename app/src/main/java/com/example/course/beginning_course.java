aad26c34fbf123fb369df971df0299d42da6d1da
Supermagzzz <12345min@mail.ru>
2019-05-09 00:10:00 +0700

    adding 4'th article in Beginning course

--- app/src/main/java/com/example/course/beginning_course.java
+++ app/src/main/java/com/example/course/beginning_course.java
@@ -45,6 +45,12 @@ public class beginning_course extends AppCompatActivity {
                 intent.putExtra("id", 3);
                 startActivity(intent);
                 break;
+            case R.id.article4:
+                intent = new Intent(beginning_course.this, Article.class);
+                intent.putExtra("site", "4-Variables.html");
+                intent.putExtra("id", 4);
+                startActivity(intent);
+                break;
             case R.id.tasks2:
                 intent = new Intent(beginning_course.this, TaskActivity.class);
                 intent.putExtra("id", 2);
